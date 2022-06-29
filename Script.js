69e885207e3ec5eedb647622a6f69baec502831c
VitorHugoSouz007 <vitorhugo6757@gmail.com>
2022-06-29 20:49:08 -0300

    versao inicial

---
+++ Script.js
@@ -0,0 +1,40 @@
+const nome            = "Vitor"
+
+const sobrenome       = "Souza"
+
+const data_nascimento = "29/08/05"
+
+let altura            = 1.83
+
+let numero_sapato     = 43
+
+let idade             = 16
+
+const celular         = "(18) 99606-0273"
+
+let cor_favorita      = 'Azul'
+
+let qtde_moto         = null
+
+let qtde_carro        = null
+
+// <- o valor
+qtde_carro            = 1
+
+altura                = 1.83
+
+celular               = "(19) 99555-0982"
+
+// comparar/validar/verificar Igualdade
+
+nome.trim().toLocaleLowerCase() == "VITOR" 
+
+idade                 = 16 //true
+
+idade                 = 15
+
+idade                 = 15 //true
+
+(true == false) == (true == false)
+
+// Igualdade
